cdd4faad560157dc82038847d8f34adaafe8beda
NekoLuna喵洛娜 <62191675+MiaoLuoNa@users.noreply.github.com>
2024-02-12 19:00:46 +0800

    新建项目

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# beijing-weather-v2
- 
+# 京城气象v2
+
+	项目v1.0：https://github.com/MiaoLuoNa/BJWeather
+BJWeather的重构版本
---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import { createApp } from "vue"
+import App from './App.vue'
+
+//引入mdui
+import 'mdui/mdui.css';
+import 'mdui';
+
+createApp(App).mount('#app')
