a0c4c679b3a730a715ab63eab07c41fbc5e6b10f
NekoLuna喵洛娜 <62191675+MiaoLuoNa@users.noreply.github.com>
2024-02-13 23:26:46 +0800

    Update BMS.vue

--- src/view/Home/components/components/BMS/BMS.vue
+++ src/view/Home/components/components/BMS/BMS.vue
@@ -1,7 +1,7 @@
 <template>
     <mdui-card>
         北京气象台
-        <mdui-tabs value="tab-1">
+        <mdui-tabs value="tab-1" class="dee">
             <mdui-tab value="tab-1">实况</mdui-tab>
             <mdui-tab value="tab-2">Tab 2</mdui-tab>
             <mdui-tab value="tab-3">Tab 3</mdui-tab>
@@ -17,8 +17,14 @@
 
 <script lang="ts" setup>
     import axios, { AxiosError } from 'axios'
-    import { ref } from 'vue'
+    import { onMounted, ref } from 'vue'
     import Scene from './components/Scene.vue'
+
+    onMounted(() => {
+        ;(document.querySelector('.dee')!.shadowRoot!.querySelector('.container')! as HTMLElement).style.backgroundColor = 'rgb(var(--mdui-color-surface-container-low))'
+    })
+
+    //nextTick，不一定要用在onMounted里
 
     //
 </script>
@@ -26,5 +32,10 @@
 <style scoped>
     mdui-card {
         width: 100%;
+    }
+
+    mdui-tab,
+    mdui-tabs {
+        background-color: rgb(var(--mdui-color-surface-container-low)) !important;
     }
 </style>
