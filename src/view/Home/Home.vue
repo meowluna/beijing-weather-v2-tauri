3d391fba7c4c20392d0f7ed0ea531bf1db71459e
NekoLuna喵洛娜 <62191675+MiaoLuoNa@users.noreply.github.com>
2024-02-13 19:22:29 +0800

    数据源组件数据持久化

--- src/view/Home/Home.vue
+++ src/view/Home/Home.vue
@@ -5,7 +5,7 @@
         <div style="flex-grow: 1"></div>
         <mdui-dropdown stay-open-on-click>
             <mdui-button-icon icon="more_vert" slot="trigger"></mdui-button-icon>
-            <mdui-menu selects="multiple" class="scoreSelects" :value="sel" @change="sel = $event.target.value">
+            <mdui-menu selects="multiple" class="scoreSelects" :value="scoreSelectsList" @change="scoreSelectsList = $event.target.value">
                 可视数据源
                 <mdui-menu-item value="bms">北京气象台</mdui-menu-item>
                 <mdui-menu-item value="item-2">Item 2</mdui-menu-item>
@@ -20,11 +20,10 @@
     import BMS from './components/BMS.vue'
     import { ref } from 'vue'
 
-    let sel: any = ref([])
+    let scoreSelectsList: any = ref(JSON.parse(localStorage.getItem('scoreSelectsList') || '')._value)
     function isScoreShow(item: any) {
-        if (sel.value.find((el: any) => el == item)) {
-            return true
-        }
+        localStorage.setItem('scoreSelectsList', JSON.stringify(scoreSelectsList))
+        return scoreSelectsList.value.find((el: any) => el == item) ? true : false
     }
 </script>
 
